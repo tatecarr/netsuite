94770d82122227acac6d95708705587aa77beb1b
Taylor Carr <taylor@catamountsolutions.com>
2020-01-29 17:58:03 -0500

    Add custom_field_list functionality to Partner record

--- lib/netsuite/records/partner.rb
+++ lib/netsuite/records/partner.rb
@@ -16,6 +16,8 @@ module NetSuite
              :partner_code, :is_person, :company_name, :eligible_for_commission, :entity_id, :last_modified_date,
              :date_created, :title, :mobile_phone, :comments, :middle_name, :send_email, :password, :password2
 
+      field :custom_field_list, CustomFieldList
+
       record_refs :klass, :access_role, :department
 
       attr_reader   :internal_id
